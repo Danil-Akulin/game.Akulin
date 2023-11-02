a7d45da372ee1cc4fda67d44b6d67df0e11c4982
Opilane TTHK <opilane@tthk.ee>
2023-11-02 14:40:40 +0200

    last ... [Delivers #pivotail-kasutajaloo-id]

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
 const express = require('express')
-const cors= require('cors')
-const app = require('express')()
-const port= 8080
-const swaggerUi = require('swagger-ui-express');
-
+const app = express()
+const cors = require('cors')
+const port = 8080
+const swaggerUI = require('swagger-ui-express')
 const yamljs = require('yamljs')
 const swaggerDocument = yamljs.load('./docs/swagger.yaml');
-
 app.use(cors())
 app.use(express.json())
-
-
-app.get('/games', (req, res) => {
-    res.send(["Witcher 3", "Cyberpunk 2077"])
-})
 
 const games = [
     {id: 1, name: "Witcher 3", price: 29.99},
@@ -31,41 +24,45 @@ app.get('/games', (req, res) => {
 })
 
 app.get('/games/:id', (req, res) => {
-    if (typeof games[req.params.id - 1] === 'undefined'){
-        return res.status(404).send({error: "Game not found"})
-    }
-    res.send(games[req.params.id - 1])
-})
-
-app.post('games', (req, res) => {
-    if(!req.body.name || !req.body.price) {
-        return res.status(400).send({error: 'One oe all params are missing' })
-    }
-    let game = {
-        id: games.length + 1,
-        price: req.body.price,
-        name: req.body.name
-    }
-
-    games.push(game)
-    
-    res.status(201)
-        .location(`${getBaseUrl(req)}/games/${games.length}`)
-        .send(game)
-})
-
-app.delete('/games/:id', (req, res) => {
     if (typeof games[req.params.id - 1] === 'undefined') {
         return res.status(404).send({error: "Game not found"})
     }
 
-    games.splice(req.params.id - 1, 1)
+    res.send(games[req.params.id - 1])
+})
+
+app.post('/games', (req, res) => {
+    if (!req.body.name || !req.body.price) {
+        return res.status(400).send({ error: 'One or all params are missing'})
+    }
+    let game = {
+        id: games.length +1,
+        price: req.body.price,
+        name: req.body.name
+    }
+    
+    games.push(game)
+
+    res.status(201).location(`${getBaseUrl(req)}/games/${games.length}`)
+        .send(game)
+})
+
+app.delete('/games/:id', (req, res) => {
+    if (typeof games[req.params.id -1] === 'undefined') {
+        return res.status(404).send({error: "Game not found"})
+    }
+
+    games.splice(req.params.id -1, 1)
 
     res.status(204).send({error: "No content"})
 })
 
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 
 app.listen(port, () => {
     console.log(`API up at: http://localhost:${port}`)
 })
+
+function getBaseUrl(req) {
+    return req.connection && req.connection.encrypted ? 'https' : 'http' + `://${req.headers.host}`
+}
